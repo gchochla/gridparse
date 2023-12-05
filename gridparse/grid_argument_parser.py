ab9493f00b4f6ac5e5844e2ec4b295d72b82af57
Georgios <georgioschochlakis@gmail.com>
2023-12-04 18:50:59 -0800

    Fix issues with subparsers

--- gridparse/grid_argument_parser.py
+++ gridparse/grid_argument_parser.py
@@ -1,11 +1,71 @@
 import argparse
-from typing import Tuple, List
+from typing import Any, Tuple, List, Optional, Union, Sequence
 from copy import deepcopy
 
 from gridparse.utils import list_as_dashed_str, strbool
 
 
-class GridArgumentParser(argparse.ArgumentParser):
+# overwritten to fix issue in __call__
+class _GridSubparserAction(argparse._SubParsersAction):
+    def __call__(
+        self,
+        parser: argparse.ArgumentParser,
+        namespace: List[argparse.Namespace],
+        values: Optional[Union[str, Sequence[Any]]],
+        option_string: Optional[str] = None,
+    ) -> None:
+        parser_name = values[0]
+        arg_strings = values[1:]
+
+        # set the parser name if requested
+        if self.dest is not argparse.SUPPRESS:
+            setattr(namespace, self.dest, parser_name)
+
+        # select the parser
+        try:
+            parser = self._name_parser_map[parser_name]
+        except KeyError:
+            args = {
+                'parser_name': parser_name,
+                'choices': ', '.join(self._name_parser_map),
+            }
+            msg = (
+                argparse._(
+                    'unknown parser %(parser_name)r (choices: %(choices)s)'
+                )
+                % args
+            )
+            raise argparse.ArgumentError(self, msg)
+
+        # parse all the remaining options into the namespace
+        # store any unrecognized options on the object, so that the top
+        # level parser can decide what to do with them
+
+        # In case this subparser defines new defaults, we parse them
+        # in a new namespace object and then update the original
+        # namespace for the relevant parts.
+        # NOTE: changed here because parser.parse_args() now returns a list
+        # of namespaces instead of a single namespace
+        subnamespaces, arg_strings = parser.parse_known_args(arg_strings, None)
+        for subnamespace in subnamespaces:
+            for key, value in vars(subnamespace).items():
+                setattr(namespace, key, value)
+
+        if arg_strings:
+            vars(namespace).setdefault(argparse._UNRECOGNIZED_ARGS_ATTR, [])
+            getattr(namespace, argparse._UNRECOGNIZED_ARGS_ATTR).extend(
+                arg_strings
+            )
+
+
+# overwritten to include our _SubparserAction
+class _GridActionsContainer(argparse._ActionsContainer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.register("action", "parsers", _GridSubparserAction)
+
+
+class GridArgumentParser(_GridActionsContainer, argparse.ArgumentParser):
     """ArgumentParser that supports grid search.
 
     It transforms the following arguments in the corresponding way:
